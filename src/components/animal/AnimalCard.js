0ae70339c74068932cbb2cf510a9a9bf9312ec1a
leigharobinson <lmarobinson24@gmail.com>
2020-06-02 12:02:13 -0500

    completed chap 6: Dynamic Routing

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Animal.css";
+import { Link } from "react-router-dom";
 const AnimalCard = (props) => {
   return (
     <div className="card">
@@ -17,6 +18,9 @@ const AnimalCard = (props) => {
         >
           Discharge
         </button>
+        <Link to={`/animals/${props.animal.id}`}>
+          <button>Details</button>
+        </Link>
       </div>
     </div>
   );
