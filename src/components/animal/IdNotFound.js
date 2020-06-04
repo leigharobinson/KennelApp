c43742ecb99d9e84e28d301039ba4517bf904189
leigharobinson <lmarobinson24@gmail.com>
2020-06-04 13:48:14 -0500

    added an image for no ID found

--- src/components/animal/IdNotFound.js
+++ src/components/animal/IdNotFound.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import PageNotFound from ".a/images/notFound.jpg";
+import pugError404 from "./images/pugError404.jpg";
 
 class NotFoundID extends React.Component {
   render() {
     return (
       <div>
         <h1>No ID match</h1>
-        {/* <img src={PageNotFound} /> */}
+        <img src={pugError404} />
         <p style={{ textAlign: "center" }}>
           <Link to="/">Go to Home </Link>
         </p>
